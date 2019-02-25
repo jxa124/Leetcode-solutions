2c85e8c09cb295ef0d0c9545d0890179f49ea07d
JingX <348744270@qq.com>
2019-02-25 22:49:18 +0800

    Update 275.py
    思路一样

--- 275.py
+++ 275.py
@@ -10,3 +10,23 @@ def hIndex_bi(citations):
         else:
             high = mid - 1
     return low-1
+# C++
+# class Solution {
+# public:
+#     int hIndex(vector<int>& citations) {
+#         int n = citations.size();
+#         int left = 0;
+#         int right = n-1;
+        
+#         while(left <= right){
+#             int mid = left + (right - left)/2 ;
+#             if (citations[mid] >= n-mid){
+#                 right = mid - 1;
+#             }
+#             else{
+#                 left = mid + 1;
+#             }
+#         }
+#         return n-left;
+#     }
+# };
