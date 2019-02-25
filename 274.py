36651c48d5b4d0790b40dc4ee3925b7ad797d0fc
JingX <348744270@qq.com>
2019-02-25 22:32:51 +0800

    Update 274.py
    【维基百科】可以按照如下方法确定某人的H指数：
    将其发表的所有SCI论文按被引次数从高到低排序；
    从前往后查找排序后的列表，直到某篇论文的序号大于该论文被引次数。所得序号减一即为H指数。
    以上为维基百科的内容。
    可以不用前面的判断，因为那两种情况包含在后面的return中了；

--- 274.py
+++ 274.py
@@ -7,3 +7,21 @@ def hIndex(citaions):
         if h+1 > c:
             return h
     return len(citaions)
+
+# C++解法
+# class Solution {
+# public:
+#     int hIndex(vector<int>& citations) {
+#         std:sort(citations.begin(), citations.end());
+#         int n  = citations.size();
+#         int index = 0;
+#         for(int i = n-1;i>=0;i--){
+#             index++;
+#             if(index > citations[i]){
+#                 return index-1;
+#             }
+#         }
+#         return n;
+        
+#     }
+# };
